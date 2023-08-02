5ab29766638f16b0fd0d1b8142d7f8f354843bbd
Igor_Mas <111005946+isoplay@users.noreply.github.com>
2023-08-02 17:38:12 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
+
+// o codigo implementa uma interação simples de rolagem na pagina e modifica algumas classes do ID "mainNav" baseada no comportamento do usuario ao rolar para cima ou para baixo que quando ele descer o scroll do mouse o cabeçalho some e quando subir ele reaparece
